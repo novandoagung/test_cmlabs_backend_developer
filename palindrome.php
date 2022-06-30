f6c34ca6d4ea9b76fd0d50cdaf625d1417161f1b
Novando Agung Syahputra <45658284+novandoagung@users.noreply.github.com>
2022-06-30 09:53:43 +0700

    Add files via upload

---
+++ palindrome.php
@@ -0,0 +1,161 @@
+<?php 
+	$input = "SAIPPUAKIVIKAUPPIAS";
+
+	$array = str_split($input);
+
+	for ($a=0; $a<count($array); $a++) { 
+	 	for($b = count($array)-$a-1; $b>=0; $b--){
+
+	 		echo $array[$a]." and ".$array[$b]."<br />";
+	 		if($array[$a]==$array[$b]){
+	 			$palindrome = "Yes";
+	 		}
+	 		else{
+	 			$palindrome = "No";
+	 			break;
+	 		}
+	 		break;
+	 	}
+	 }
+	 if ($palindrome=="Yes") {
+	  	echo "Palindrome Benar<br />";
+	  }
+	 if ($palindrome=="No") {
+	  	echo "Palindrome Salah<br />";
+	  } 
+ ?>
+
+ <?php 
+	$input = "Aibohphobia";
+
+	$array = str_split($input);
+
+	for ($a=0; $a<count($array); $a++) { 
+	 	for($b = count($array)-$a-1; $b>=0; $b--){
+
+	 		echo $array[$a]." and ".$array[$b]."<br />";
+	 		if($array[$a]==$array[$b]){
+	 			$palindrome = "Yes";
+	 		}
+	 		else{
+	 			$palindrome = "No";
+	 			break;
+	 		}
+	 		break;
+	 	}
+	 }
+	 if ($palindrome=="Yes") {
+	  	echo "Palindrome Benar<br />";
+	  }
+	 if ($palindrome=="No") {
+	  	echo "Palindrome Salah<br />";
+	  } 
+ ?>
+
+ <?php 
+	$input = "Anna";
+
+	$array = str_split($input);
+
+	for ($a=0; $a<count($array); $a++) { 
+	 	for($b = count($array)-$a-1; $b>=0; $b--){
+
+	 		echo $array[$a]." and ".$array[$b]."<br />";
+	 		if($array[$a]==$array[$b]){
+	 			$palindrome = "Yes";
+	 		}
+	 		else{
+	 			$palindrome = "No";
+	 			break;
+	 		}
+	 		break;
+	 	}
+	 }
+	 if ($palindrome=="Yes") {
+	  	echo "Palindrome Benar<br />";
+	  }
+	 if ($palindrome=="No") {
+	  	echo "Palindrome Salah<br />";
+	  } 
+ ?>
+
+ <?php 
+	$input = "Civic";
+
+	$array = str_split($input);
+
+	for ($a=0; $a<count($array); $a++) { 
+	 	for($b = count($array)-$a-1; $b>=0; $b--){
+
+	 		echo $array[$a]." and ".$array[$b]."<br />";
+	 		if($array[$a]==$array[$b]){
+	 			$palindrome = "Yes";
+	 		}
+	 		else{
+	 			$palindrome = "No";
+	 			break;
+	 		}
+	 		break;
+	 	}
+	 }
+	 if ($palindrome=="Yes") {
+	  	echo "Palindrome Benar<br />";
+	  }
+	 if ($palindrome=="No") {
+	  	echo "Palindrome Salah<br />";
+	  } 
+ ?>
+
+ <?php 
+	$input = "My gym";
+
+	$array = str_split($input);
+
+	for ($a=0; $a<count($array); $a++) { 
+	 	for($b = count($array)-$a-1; $b>=0; $b--){
+
+	 		echo $array[$a]." and ".$array[$b]."<br />";
+	 		if($array[$a]==$array[$b]){
+	 			$palindrome = "Yes";
+	 		}
+	 		else{
+	 			$palindrome = "No";
+	 			break;
+	 		}
+	 		break;
+	 	}
+	 }
+	 if ($palindrome=="Yes") {
+	  	echo "Palindrome Benar<br />";
+	  }
+	 if ($palindrome=="No") {
+	  	echo "Palindrome Salah<br />";
+	  } 
+ ?>
+
+ <?php 
+	$input = "No lemon, no melon";
+
+	$array = str_split($input);
+
+	for ($a=0; $a<count($array); $a++) { 
+	 	for($b = count($array)-$a-1; $b>=0; $b--){
+
+	 		echo $array[$a]." and ".$array[$b]."<br />";
+	 		if($array[$a]==$array[$b]){
+	 			$palindrome = "Yes";
+	 		}
+	 		else{
+	 			$palindrome = "No";
+	 			break;
+	 		}
+	 		break;
+	 	}
+	 }
+	 if ($palindrome=="Yes") {
+	  	echo "Palindrome Benar<br />";
+	  }
+	 if ($palindrome=="No") {
+	  	echo "Palindrome Salah<br />";
+	  } 
+ ?>
